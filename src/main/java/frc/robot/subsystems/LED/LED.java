57a8cae75fe6732c5d1c03a82c9749bcf8ff9f40
DesWes6 <Brycen.Joseph281@gmail.com>
2026-02-22 14:31:49 -0500

    Sunday work

--- src/main/java/frc/robot/subsystems/LED/LED.java
+++ src/main/java/frc/robot/subsystems/LED/LED.java
@@ -2,6 +2,10 @@ package frc.robot.subsystems.LED;
 
 import static frc.robot.subsystems.LED.LEDConstants.*;
 
+import org.ejml.equation.Variable;
+
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.intake.Intake;
@@ -9,6 +13,7 @@ import frc.robot.subsystems.intake.Intake;
 public class LED extends SubsystemBase {
     private final Spark blinkin;
     private final Intake intake;
+    private double teamColor;
 
     public LED(Intake intake) {
         blinkin = new Spark(blinkinPWMPort);
@@ -22,9 +27,16 @@ public class LED extends SubsystemBase {
     @Override
     public void periodic() {
         if (intake.getIsRunning()) {
-            setPattern(solidGreen);
+            setPattern(solidBlue);
         } else {
-            setPattern(solidGray);
+            setPattern(solidRed);
         }
+    }
+    Variable alliance = DriverStation.getAlliance();
+    if (alliance.isPresent() && alliance.get() == Alliance.Red){
+        teamColor = solidBlue;
+    }
+    else {
+        teamColor = solidRed;
     }
 }
--- src/main/java/frc/robot/subsystems/LED/LEDConstants.java
+++ src/main/java/frc/robot/subsystems/LED/LEDConstants.java
@@ -4,7 +4,7 @@ public class LEDConstants {
     public static final int blinkinPWMPort = 2;
 
     // Solid color values (from REV Blinkin pattern table)
-    public static final double solidGray = 0.98;
+    public static final double solidRed = 0.61;
+    public static final double solidBlue = 0.87;
     public static final double solidGreen = 0.77;
-
 }
