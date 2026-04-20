d95c24738452d6d0456728027bfd54e623fabbf7
Brycen2811 <abbyzodun@gmail.com>
2026-04-20 19:43:25 -0400

    even more autos

--- src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
@@ -7,6 +7,6 @@ public class IntakeConstants {
   public static final double deployRangeError = 0.2;
   public static final double intakeDeploySpeed = -0.15;
   public static final double intakeSpeed = 0.8; //was 0.8
-  public static final double intakeLiftScale = 0.4;
+  public static final double intakeLiftScale = 0.35;
   public static final double intakeShakeSpeed = -0.12;
 }
